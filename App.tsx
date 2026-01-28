bc9c4b06d70279c6b79e46ceda472917e8ef1df6
Kishore <kishore@MacBook-Air.local>
2026-01-28 13:43:18 +0530

    Added text import

--- App.tsx
+++ App.tsx
@@ -6,11 +6,12 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View , Text} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
